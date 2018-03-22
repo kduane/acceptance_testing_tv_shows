68501188ddc2a9be62fcfd9d6fc698ead189b086
kduane <karl.duane@gmail.com>
2018-03-22 16:57:20 -0400

    First Commit

---
+++ server.rb
@@ -0,0 +1,6 @@
+require 'sinatra'
+require 'csv'
+require_relative "app/models/television_show"
+
+set :bind, '0.0.0.0'  # bind to all interfaces
+set :views, File.join(File.dirname(__FILE__), "app/views")

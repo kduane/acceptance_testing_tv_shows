d8026860f3a2f12e00d725374e53dd2809eb922e
kduane <karl.duane@gmail.com>
2018-03-23 12:46:43 -0400

    added get '/television_shows', post '/television_show', and the initialization of TelevisionShow class

--- app/models/television_show.rb
+++ app/models/television_show.rb
@@ -1,3 +1,12 @@
 class TelevisionShow
   GENRES = ["Action", "Mystery", "Drama", "Comedy", "Fantasy"]
+
+  def initialize(title, network, starting_year, synopsis, genre)
+    @title = title
+    @network = network
+    @starting_year = starting_year
+    @synopsis = synopsis
+    @genre = genre
+  end
+  
 end
--- server.rb
+++ server.rb
@@ -4,3 +4,27 @@ require_relative "app/models/television_show"
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 set :views, File.join(File.dirname(__FILE__), "app/views")
+
+get '/television_shows' do
+  @television_shows = []
+
+  CSV.foreach("television_shows.csv", :headers => true, :col_sep = ',') do |row|
+    @television_shows << Television_Show.new(:title => row['title'], :network => row['network'], :starting_year => row['starting
+      '], :synopsis => row['synopsis'], :genre => row['genre'])
+  end
+
+  erb :index
+end
+
+get '/television_shows/new' do
+  erb :new
+end
+
+post '/television_show' do
+  title = params['title']
+  network = params['network']
+  starting_year = params['starting_year']
+  synopsis = params['synopsis']
+  genre = params['genre']
+  CSV.open("television_shows.csv", "a+")
+end

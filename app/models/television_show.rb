c27a93040cff201bb9f2739deb7f5837b2d3925b
kduane <karl.duane@gmail.com>
2018-03-23 14:20:31 -0400

    added .summary method to TelevisionShow class

--- app/models/television_show.rb
+++ app/models/television_show.rb
@@ -1,5 +1,6 @@
 class TelevisionShow
   GENRES = ["Action", "Mystery", "Drama", "Comedy", "Fantasy"]
+  attr_reader :title, :network, :starting_year, :synopsis, :genre
 
   def initialize(title, network, starting_year, synopsis, genre)
     @title = title
@@ -7,6 +8,10 @@ class TelevisionShow
     @starting_year = starting_year
     @synopsis = synopsis
     @genre = genre
+  end
+
+  def summary
+    "Title: #{@title} \n Network: #{@network}\n Starting Year: #{@starting_year}\n Synopsis: #{@synopsis}\n Genre: #{@genre}"
   end
 
 end

c1d8462b4418bd0470afb89079f9e28e39ff8e5e
kduane <karl.duane@gmail.com>
2018-03-26 12:05:36 -0400

    added sinatra reloader, added sessions for 1-time messages

--- server.rb
+++ server.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 require 'csv'
 require_relative "app/models/television_show"
+require 'pry'
+require "pry" if development? || test?
+require "sinatra/reloader" if development?
 
 set :bind, '0.0.0.0'  # bind to all interfaces
 set :views, File.join(File.dirname(__FILE__), "app/views")
+se Rack::Session::Cookie, {
+  secret: "keep_it_secret_keep_it_safe",
+}
 
 get '/television_shows' do
   @television_shows = []
@@ -20,14 +26,19 @@ get '/television_shows/new' do
 end
 
 post '/television_show' do
-  title = params['title']
-  network = params['network']
-  starting_year = params['starting_year']
-  synopsis = params['synopsis']
-  genre = params['genre']
   # binding.pry
-  CSV.open("television-shows.csv", "a+") do |csv|
-    csv << [title, network, starting_year, synopsis, genre]
+  if params.values.any?{|i| i.empty?}
+    @message = "Please fill in all required fields"
+    redirect '/television_shows/new'
+  else
+    title = params[:title]
+    network = params[:network]
+    starting_year = params[:starting_year]
+    synopsis = params[:synopsis]
+    genre = params[:genre]
+    CSV.open("television-shows.csv", "a+") do |csv|
+      csv << [title, network, starting_year, synopsis, genre]
+    end
+    redirect '/television_shows'
   end
-  redirect '/television_shows'
 end
